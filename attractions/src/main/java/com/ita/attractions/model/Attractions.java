e7237e45b80b363a9835666a28aeff6d873b7236
acvijic <aleksa.cvijic@student.um.si>
2024-04-14 23:55:32 +0200

    Endpoint Test

--- attractions/src/main/java/com/ita/attractions/model/Attractions.java
+++ attractions/src/main/java/com/ita/attractions/model/Attractions.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class Attractions {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "attractions_generator")
+    @SequenceGenerator(name = "attractions_generator", sequenceName = "attractions_seq", allocationSize = 1)
     private Long id;
 
     private String name;
--- attractions/src/test/java/com/ita/attractions/repository/AttractionsRepositoryTests.java
+++ attractions/src/test/java/com/ita/attractions/repository/AttractionsRepositoryTests.java
@@ -1,7 +1,6 @@
-package com.ita.attractions;
+package com.ita.attractions.repository;
 
 import com.ita.attractions.model.Attractions;
-import com.ita.attractions.repository.AttractionsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
