cce75d54a90529833896291298ad9b2dbca2f86c
acvijic <aleksa.cvijic@student.um.si>
2024-05-19 16:56:49 +0200

    reformatting

--- micro-fe/subscriptions-frontend/src/root.js
+++ micro-fe/subscriptions-frontend/src/root.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AddAttraction from "./components/AddAttraction";
 import AttractionsList from "./components/AttractionsList";
+import Subscription from "./components/Subscription";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost";
@@ -8,9 +9,10 @@ axios.defaults.baseURL = "http://localhost";
 const Root = () => {
   return (
     <div>
-      <h1>Hello from Attractions!</h1>
+      <h1>Hello from Micro Frontends!</h1>
       <AddAttraction />
       <AttractionsList />
+      <Subscription />
     </div>
   );
 };
